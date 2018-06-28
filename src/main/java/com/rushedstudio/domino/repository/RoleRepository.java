b765e76ee6ccc40d9dfdd1a3843c24aeec036df3
Krzysztof Pośpiech <christoferrush@gmail.com>
2018-06-28 13:53:36 +0200

    made beans from repositories

--- src/main/java/com/rushedstudio/domino/repository/RoleRepository.java
+++ src/main/java/com/rushedstudio/domino/repository/RoleRepository.java
@@ -1,8 +1,9 @@
 package com.rushedstudio.domino.repository;
 
-import com.rushedstudio.domino.model.Role;
+import com.rushedstudio.domino.domain.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 }
--- src/main/java/com/rushedstudio/domino/repository/UserRepository.java
+++ src/main/java/com/rushedstudio/domino/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package com.rushedstudio.domino.repository;
 
-import com.rushedstudio.domino.model.User;
+import com.rushedstudio.domino.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
