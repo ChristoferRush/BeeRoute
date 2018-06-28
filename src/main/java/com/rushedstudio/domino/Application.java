4a3b01ad47624854cdd113c17e4ad7e03ad38c92
Krzysztof Pośpiech <christoferrush@gmail.com>
2018-06-28 13:04:24 +0200

    repaired configuration

--- src/main/java/com/rushedstudio/domino/Application.java
+++ src/main/java/com/rushedstudio/domino/Application.java
@@ -1,9 +1,9 @@
-package com.rushedstudio.domino.app;
+package com.rushedstudio.domino;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.rushedstudio.domino")
 public class Application {
 
     public static void main(String[] args){
