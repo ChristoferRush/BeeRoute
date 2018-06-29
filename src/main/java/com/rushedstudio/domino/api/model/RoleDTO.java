46f15bbc6892001eb2668e6982b2266c1361a144
Krzysztof Pośpiech <christoferrush@gmail.com>
2018-06-29 10:04:36 +0200

    repaired DTO

--- src/main/java/com/rushedstudio/domino/api/model/RoleDTO.java
+++ src/main/java/com/rushedstudio/domino/api/model/RoleDTO.java
@@ -3,13 +3,13 @@ package com.rushedstudio.domino.api.model;
 import com.rushedstudio.domino.domain.Permission;
 import lombok.Data;
 
-import java.util.Set;
+import java.util.List;
 
 @Data
 public class RoleDTO {
 
     private Long id;
     private String name;
-    private Set<UserDTO> users;
+    private List<UserDTO> users;
     private Permission permission;
 }
--- src/main/java/com/rushedstudio/domino/api/model/UserDTO.java
+++ src/main/java/com/rushedstudio/domino/api/model/UserDTO.java
@@ -1,4 +1,14 @@
 package com.rushedstudio.domino.api.model;
 
+import com.rushedstudio.domino.domain.Role;
+
 public class UserDTO {
+
+    private Long id;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Role role;
 }
